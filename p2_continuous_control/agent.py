617d8c0e837664567ddc995f44353b764877d882
Jeremy Vila <Jeremy.Vila@shell.com>
2018-10-16 20:43:06 -0500

    Added agent code to the repo. This agent has a standard neural network as the policy.

---
+++ p2_continuous_control/agent.py
@@ -0,0 +1,7 @@
+# This is the agent for the continuous control problem.
+# The environment is defined by the Unity engine
+#
+# This agent controls a robotic arm with 12 degrees of freedom
+#
+# Coded by Jeremy Vila
+# 10/20/18
