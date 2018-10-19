94faae645c29c9baf0dc80e4a782a02ec98a258b
Jeremy Vila <Jeremy.Vila@shell.com>
2018-10-18 19:08:52 -0500

    Added nn model to agent.  Currently full connected.

--- p2_continuous_control/agent.py
+++ p2_continuous_control/agent.py
@@ -13,5 +13,32 @@ import torch.nn.functional as functional
 
 class Agent(nn.Module):
 
-	def __init__(self):
-		super(Agent, self).__init__()
+	def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(Agent, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        
+        hidden_sizes = [32, 16]
+        dropout_rates = [.2, .2]
+
+        self.fc1 = nn.Linear(state_size, hidden_sizes[0])
+        self.fc2 = nn.Linear(hidden_sizes[0], hidden_sizes[1])
+        self.fc3 = nn.Linear(hidden_sizes[1], action_size)
+
+        # Dropout considered, but not needed!
+        self.drop1 = nn.Dropout(p=dropout_rates[0])
+        self.drop2 = nn.Dropout(p=dropout_rates[1])
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        # x = self.drop1(x)
+        x = F.relu(self.fc2(x))
+        # x = self.drop2(x)
+        return self.fc3(x)
