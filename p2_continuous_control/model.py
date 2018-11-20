ab20d7231ddacd7345c815525cb1e5b50daa7e84
Jeremy Vila <Jeremy.Vila@shell.com>
2018-11-19 20:02:00 -0600

    feat: Added ability to save and load actor and critic models.

--- p2_continuous_control/model.py
+++ p2_continuous_control/model.py
@@ -1,3 +1,12 @@
+'''
+Class definitions for the Actor and Critic for the ddpg network for the Reacher problem
+in the Udacity Deep Learning Nano degree course.
+
+Some methods taken from DDPG file in Udacity Deep Learning course
+Coded by: Jeremy Vila
+11/13/18
+'''
+
 import numpy as np
 
 import torch
@@ -9,66 +18,63 @@ def hidden_init(layer):
     lim = 1. / np.sqrt(fan_in)
     return (-lim, lim)
 
+# class definition for the actor
 class Actor(nn.Module):
-    """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc_units=256):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-            fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer
-        """
+    def __init__(self, state_size, action_size, seed, fc_units=[256, 128]):
+
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc_units)
-        self.fc2 = nn.Linear(fc_units, action_size)
-        self.reset_parameters()
+        self.fc1 = nn.Linear(state_size, fc_units[0])
+        self.fc2 = nn.Linear(fc_units[0], fc_units[1])
+        self.fc3 = nn.Linear(fc_units[1], action_size)
+        self.reset_param()  # Reset parameters for the model
 
-    def reset_parameters(self):
+    # method to have a hard coding of parameters
+    def reset_param(self):
         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-1e-3, 1e-3)
+        # Adding bias seems to help here ...
+        self.fc1.bias.data.fill_(0.05)
+        self.fc2.bias.data.fill_(0.05)
+        self.fc3.bias.data.fill_(0.05)   
 
+    #  Three layer network
     def forward(self, state):
-        """Build an actor (policy) network that maps states -> actions."""
         x = F.relu(self.fc1(state))
-        return F.tanh(self.fc2(x))
+        x = F.relu(self.fc2(x))
+        return torch.tanh(self.fc3(x))
 
-
+# class definition for the critic
 class Critic(nn.Module):
-    """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=256, fc2_units=256, fc3_units=128):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-            fcs1_units (int): Number of nodes in the first hidden layer
-            fc2_units (int): Number of nodes in the second hidden layer
-        """
+    def __init__(self, state_size, action_size, seed, fc_units=[256, 256, 128]):
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fcs1 = nn.Linear(state_size, fcs1_units)
-        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, fc3_units)
-        self.fc4 = nn.Linear(fc3_units, 1)
-        self.reset_parameters()
+        self.fc1 = nn.Linear(state_size, fc_units[0])
+        self.fc2 = nn.Linear(fc_units[0] + action_size, fc_units[1])
+        self.fc3 = nn.Linear(fc_units[1], fc_units[2])
+        self.fc4 = nn.Linear(fc_units[2], 1)
+        self.reset_param()  # Reset the parameters
 
-    def reset_parameters(self):
-        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
+    # Reset paramers
+    def reset_param(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
         self.fc4.weight.data.uniform_(-3e-3, 3e-3)
+        # Adding bias seems to help ...
+        self.fc1.bias.data.fill_(0.05)
+        self.fc2.bias.data.fill_(0.05)
+        self.fc3.bias.data.fill_(0.05) 
+        self.fc4.bias.data.fill_(0.05)
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = F.leaky_relu(self.fcs1(state))
+        xs = F.relu(self.fc1(state))
+        # Concatenate hidden layer and actions
         x = torch.cat((xs, action), dim=1)
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
         return self.fc4(x)
---
+++ p2_continuous_control/train.py
@@ -0,0 +1,92 @@
+'''
+Main script to train the sspg algoirthm on the Reacher problem.
+
+Some methods taken from DDPG file in Udacity Deep Learning course
+Coded by:
+Jeremy Vila
+11/13/18
+
+'''
+
+from unityagents import UnityEnvironment
+from ddpg_agent import Agent
+from collections import deque
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+# Load the environment 
+env = UnityEnvironment(file_name='/home/jeremy/projects/DeepRL_proj/p2_continuous_control/Reacher_Linux_NoVis/Reacher.x86_64')
+
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+
+# get number of agenets.  Confirm there is one
+num_agents = len(env_info.agents)
+# Get action size
+action_size = brain.vector_action_space_size
+
+# examine the state space 
+states = env_info.vector_observations
+state_size = states.shape[1]
+
+agent = Agent(state_size=state_size, action_size=action_size, random_seed=0)
+# agent = agent.load_weights()
+
+episodes = 2000  # Number of episodes
+max_time = 1000  # Max number of time steps per episode
+max_score = 32.  # average score to beat
+
+# Score lists
+scores_deque = deque(maxlen=100)
+all_scores = []
+
+# Main training loop
+for ep in range(0, episodes):
+	env_info = env.reset(train_mode=True)[brain_name]
+	states = env_info.vector_observations
+
+	# state = env.reset()
+	agent.reset()  # Resets the noise in the agent
+	scores = np.zeros(num_agents)
+	# Step through time steps and learn the actor and critic
+	for t in range(max_time):
+
+		actions = agent.act(states)									# Get actions from policy (for each agent)
+		env_info = env.step(actions)[brain_name]           			# Perform actions in environment
+		next_states = env_info.vector_observations					# get next state (for each agent)
+		rewards = env_info.rewards                         			# get reward (for each agent)
+		dones = env_info.local_done									# get dones (for each agent)
+		agent.step(states, actions, rewards, next_states, dones)	# Add experience to buffer
+		states = next_states										# Reset states
+		scores += env_info.rewards									# Accumulate rewards
+		if np.any(dones):
+			break 
+
+	scores_deque.append(np.mean(scores))
+	all_scores.append(np.mean(scores))  # Add to total list of scores
+
+	# Print results as they are computed
+	mn_score = np.mean(scores_deque)
+	print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(ep+1, mn_score, np.mean(scores)), end="")
+	if ep+1 % 100 == 0 or mn_score > max_score:
+		torch.save({'local': agent.actor_local.state_dict(),
+					'target': agent.actor_target.state_dict(),
+					'opt': agent.actor_optimizer.state_dict()}, 'cc_actor.pth')
+		torch.save({'local': agent.critic_local.state_dict(),
+					'target': agent.critic_target.state_dict(),
+					'opt': agent.critic_optimizer.state_dict()}, 'cc_critic.pth')
+	if mn_score > max_score:
+		break
+
+# plot the scores
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(len(all_scores)), all_scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.savefig("trained_scores.png")
